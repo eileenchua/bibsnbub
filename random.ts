89d4646923ef66006355b7418a13846b0eece951
Eileen Chua <22950875+eileenchua@users.noreply.github.com>
2025-11-01 04:15:46 +0800

    chore: log values

---
+++ random.ts
@@ -0,0 +1,13 @@
+// import Data from './nowhere';
+
+// const hello = 'world';
+
+// debugger;
+
+// console.warn('world', hello, Data);
+
+// function processData(data) { // 'data' is implicitly 'any'
+//   console.warn(data.toUpperCase());
+// }
+
+// processData('hello');
